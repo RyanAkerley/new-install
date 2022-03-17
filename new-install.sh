65f0f0b8f1d72f6f9bd6a3b888875e586599966f
Ryan Akerley <ryanakerley@gmail.com>
2022-03-17 06:22:19 -0400

    started new-install script

--- new-install.sh
+++ new-install.sh
@@ -1,20 +1,5 @@
-#!/bin/sh
+#! /bin/sh
 
-apt-get --assume-yes update
-apt-get --assume-yes dist-upgrade
 
-apt-get --assume-yes install binutils
-apt-get --assume-yes install clang
-apt-get --assume-yes install cmake
-apt-get --assume-yes install git
-apt-get --assume-yes install gnuradio
-apt-get --assume-yes install ipython3
-apt-get --assume-yes install lldb
-apt-get --assume-yes install llvm
-apt-get --assume-yes install python3-venv python3-pip
-apt-get --assume-yes install sl
-apt-get --assume-yes install slack
-apt-get --assume-yes install tmux
-apt-get --assume-yes install vim
-apt-get --assume-yes install wine
+install-pkgs.sh
 
