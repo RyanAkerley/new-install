55011ab0e9078cfab16eabb599ac7b791449efc0
Ryan Akerley <ryanakerley@gmail.com>
2022-03-20 19:13:57 -0400

    reorganizing

--- new-install.sh
+++ new-install.sh
@@ -5,12 +5,16 @@
 # Ryan Akerley
 
 # add sud.sh to /usr/sbin
-mv ./sud.sh /usr/sbin/sud.sh
+mv ./scripts/sud.sh /usr/sbin/
 
 #run sud.sh to update packages and repositories
-sud.sh
 chmod 754 /usr/sbin/sud.sh
+sud.sh
 
 # run install-pkgs.sh to install packages I use
 install-pkgs.sh
+
+# move backup.sh to /usr/sbin
+mv ./scripts/backup.sh /usr/sbin/
+
 
