839d15d02eb74fb830708454d7d6259ecb55aa9d
Ryan Akerley <ryanakerley@gmail.com>
2022-10-07 21:42:27 -0400

    Fleshed out the backup.sh script to create a backup in a date-labeled directory

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,8 +4,18 @@
 #
 # Ryan Akerley
 
-readonly SOURCEDIR=($1)
-readonly DESTDIR=($2)
+if [ -z $1 ]; then
+	echo "usage: backup.sh DESTINATION_DIRECTORY"
+	exit 1
+fi
 
-echo "source=" $SOURCEDIR " dest=" $DESTDIR
+readonly DESTDIR=($1)
+readonly DATE="$(date +%Y_%m_%d)"
+readonly BUPDIR=$DESTDIR/$DATE/
+echo "Destination: $BUPDIR"
 
+if [ "$(ls -A $DESTDIR)" ]; then
+	rsync -ahv --exclude=.cache/ --link-dest="$DESTDIR/$(ls -A -t $DESTDIR | head -1)" ~/ $BUPDIR
+else
+	rsync -ahv --exclude=.cache/ ~/ $BUPDIR
+fi
